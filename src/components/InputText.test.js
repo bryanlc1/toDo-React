bd382647fcf945e603ed0669b1dce4c6232bec88
bryanlc <bryanlc9808@gmail.com>
2021-11-09 16:44:30 +0100

    add test render list component

--- src/components/InputText.test.js
+++ src/components/InputText.test.js
@@ -1,9 +1,8 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import { mount } from 'enzyme';
 import InputText from './InputText';
 import List from './List';
-import { render, screen } from '../utils/test.utils';
+import { render, screen, fireEvent } from '../utils/test.utils';
 
 describe('Given a todo InputText component', () => {
   beforeEach(() => {
@@ -16,5 +15,27 @@ describe('Given a todo InputText component', () => {
 
   test('Then update button should be in the document', () => {
     expect(screen.getByTestId('update-button')).toBeInTheDocument();
+  });
+
+  describe('and the input changes', () => {
+    beforeEach(() => {
+      const taskInput = screen.getByTestId('input-task');
+      const setInputValue = 'do todo list';
+      const setIndex = '';
+      fireEvent.change(taskInput, { target: { value: setInputValue } });
+
+      render(<List setInputValue={setInputValue} setIndex={setIndex} />);
+    });
+
+    describe('And create button is clicked', () => {
+      beforeEach(() => {
+        const createButton = screen.getByTestId('create-button');
+        fireEvent.click(createButton);
+      });
+      test('Then list-item-0 should be in the document', () => {
+        const li = screen.getByTestId('list-item-0');
+        expect(li).toBeInTheDocument();
+      });
+    });
   });
 });
